ee58359468f8655c2116d3a07334a5a158914020
Anhelina Gordieieva <angelina.gordeeva10@gmail.com>
2024-11-13 19:06:41 +0200

    Добавьте файлы проекта.

---
+++ lesson15.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include<map>
+#include <string>
+#include <sstream>
+using namespace std;
+int main()
+{
+    map<string, int> mymap;
+    string text,word;
+    cout << "Input text : ";
+    getline(cin, text);
+    stringstream ss(text);
+    while (ss >> word) {
+        mymap[word]++;
+    }
+    map<string, int>::iterator it;
+    it = mymap.begin();
+    while (it != mymap.end()) 
+    {
+        cout << it->first << ": " << it->second << " "; 
+        it++; 
+    }
+    
+}
